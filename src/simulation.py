18a9ce7380a4ddb2a38055cce8a224f893dbccd4
sajji <sajjadkazemi.e@gmail.com>
2024-06-02 21:02:37 -0400

    Update Simulation class to use step method directly

--- src/simulation.py
+++ src/simulation.py
@@ -10,6 +10,7 @@ class Simulation:
         self.history = []
         self.figure = plt.figure()
         self.ax = self.figure.add_subplot(111)
+        self.run = False
 
     
     def step(self, v, w, z):
@@ -20,12 +21,15 @@ class Simulation:
         self.robot.step(v, w, z, self.dt)
         self.time += self.dt
         self.history.append(self.robot.get_pose())
+        if not self.run:
+            self.plot()
 
     def run(self, v, w, z, duration):
         """
         Run the simulation for a given duration with the given
         rotation speeds for each wheel.
         """
+        self.run = True
         for _ in range(int(duration / self.dt)):
             self.step(v, w, z)
             self.plot()
@@ -33,10 +37,10 @@ class Simulation:
     def plot(self):
         self.ax.cla()
         history = np.array(self.history)
-        self.ax.plot(history[:, 0], history[:, 1])
-        self.ax.axis('equal')
-        self.ax.set_xlim(-10, 10)
-        self.ax.set_ylim(-10, 10)
+        self.ax.plot(history[:, 0], history[:, 1], linewidth=2, color='red')
+        self.ax.set_xlim(-60, 60)
+        self.ax.set_ylim(-60, 60)
+        #self.ax.axis('equal')
         self.ax.set_xlabel('x')
         self.ax.set_ylabel('y')
         self.ax.set_title('Robot Trajectory')
@@ -51,6 +55,7 @@ class Simulation:
         side_length = self.robot.wheel_distance
         height = np.sqrt(3) / 2 * side_length  # Height of the equilateral triangle
         # Vertices of the equilateral triangle
+        centre = np.array([0, 0])
         vertices = np.array([
             [-side_length/2, -height/3],  # First vertex at origin
             [side_length/2, -height/3],  # Second vertex
@@ -63,10 +68,12 @@ class Simulation:
             [np.cos(theta), -np.sin(theta)],
             [np.sin(theta), np.cos(theta)]
         ])
+        transformed_centre = 10 * centre @ R.T + np.array([[self.robot.x, self.robot.y]])
         transformed_vertices = 10 * vertices @ R.T + np.array([[self.robot.x, self.robot.y]])
 
         # Close the triangle by adding the first vertex at the end
         triangle = np.vstack([transformed_vertices, transformed_vertices[0]])
+        self.ax.plot(transformed_centre[0,0], transformed_centre[0,1], 'ro')
         self.ax.plot(triangle[:, 0], triangle[:, 1], 'bo-', linewidth=2) 
         self.ax.fill(triangle[:, 0], triangle[:, 1], 'skyblue', alpha=0.5)
         self.ax.grid(True)
